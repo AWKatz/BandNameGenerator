7e9a0dcf66a6fc52d2c5addb0f1cd11adee606e9
Aaron <aaron.katz.mobile@gmail.com>
2023-03-19 14:44:55 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is day 1 of 100 in the Python Bootcamp
+#State the purpose of the program
+print("Lets generate a band name for your cool new side gig! ")
+
+#Get the users input for the city
+city = input("What city did you grow up in?\n")
+
+print(city + "? Woah no way! I also grew up in " +city)
+
+#Get the users input for a pet name
+pet = input("Ok...easier question, what was your favorite pets name?\n")
+
+print(pet + " what a great name!")
+
+print("Your side gig could use the name " + city + ' ' + pet + " for its merch!")
+
